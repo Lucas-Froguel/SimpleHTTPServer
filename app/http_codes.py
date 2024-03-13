cdc866b22d73a92f5660ce423bbee4cac81946c1
Lucas Froguel <lfroguel@yipitdata.com>
2024-03-13 15:46:12 -0300

    pass 2nd stage

---
+++ app/http_codes.py
@@ -0,0 +1,3 @@
+
+HTTP_200_OK = b"HTTP/1.1 200 OK\r\n\r\n"
+
--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 
 import socket
 
+from app.http_codes import HTTP_200_OK
+
+ADDRESS = "localhost"
+PORT = 4221
+
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+    server_socket = socket.create_server((ADDRESS, PORT), reuse_port=True)
+    while True:
+        conn, addr = server_socket.accept()
+        if conn:
+            conn.send(HTTP_200_OK)
+            conn.close()
 
 
 if __name__ == "__main__":
