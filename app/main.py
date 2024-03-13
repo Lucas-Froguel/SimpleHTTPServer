acc5303db7b5443b9de71c05fda6b6296ba5154a
Lucas Froguel <lfroguel@yipitdata.com>
2024-03-13 16:26:03 -0300

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@
 import socket
 
 from app.http_codes import HTTP_200_OK, HTP_404_NOT_FOUND
+from app.urls import valid_urls
 
 ADDRESS = "localhost"
 PORT = 4221
+ENCODE_TYPE = "utf-8"
 
 
 def main():
@@ -14,20 +16,25 @@ def main():
         if conn:
             data = conn.recv(4096)
             data = data.split(b" ")
-            method, path = data[0], data[1]
+            method, url = data[0], data[1]
+            msg = url.split(b"/")[2]
 
-            if is_path_valid(path):
-                conn.send(HTTP_200_OK)
-            else:
-                conn.send(HTP_404_NOT_FOUND)
+            conn.send(HTTP_200_OK + build_response(msg))
             conn.close()
 
 
-def is_path_valid(path):
-    valid_paths = [b"/"]
-    if path in valid_paths:
+def is_url_valid(url: bytes) -> bool:
+    if url in valid_urls:
         return True
     return False
+
+
+def build_response(message: bytes) -> bytes:
+    response_message = (
+        f"Content-Type: text/plain\r\nContent-Length: {len(message)}\r\n\r\r\n{message.decode(ENCODE_TYPE)}\r\n"
+    )
+
+    return response_message.encode(ENCODE_TYPE)
 
 
 if __name__ == "__main__":
