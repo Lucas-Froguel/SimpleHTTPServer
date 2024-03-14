5bcca235ad177115034681bf2163650a686623bd
Lucas Froguel <lfroguel@yipitdata.com>
2024-03-14 00:34:14 -0300

    pass 5th stage

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 
 from app.router import Router
-from app.views import index, echo
+from app.views import index, echo, user_agent
 
 
 router = Router()
 
 router.register("", index)
 router.register("echo", echo)
+router.register("user-agent", user_agent)
 
--- app/views.py
+++ app/views.py
@@ -18,3 +18,14 @@ def echo(data: bytes) -> (bytes, bytes):
 
     return HTTP_STATUS.HTTP_200_OK, response_message.encode(ENCODE_TYPE)
 
+
+def user_agent(data: bytes) -> (bytes, bytes):
+    split_data = data.split(b"\r\n")
+
+    msg = split_data[2].split(b" ")[1]
+
+    response_message = (
+        f"Content-Type: text/plain\r\nContent-Length: {len(msg)}\r\n\r\n{msg.decode(ENCODE_TYPE)}\r\n"
+    )
+
+    return HTTP_STATUS.HTTP_200_OK, response_message.encode(ENCODE_TYPE)
